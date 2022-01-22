c69289537f8dce4a886c2c31820ac0c20b4bbd21
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-01-23 00:22:05 +0800

    Create middleware to handle custom error

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 
-dotenv.config();
-connectDB();
+dotenv.config(notFound);
+connectDB(errorHandler);
 const app = express();
 
 app.get("/", (req, res) => {
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+//error not found middleware
+app.use(notFound);
+
+// error middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
