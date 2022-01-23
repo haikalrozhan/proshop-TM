d233611cc2c042a1506770462dca0635fdac50e8
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-01-23 10:57:06 +0800

    Bring redux state into HomeScreen useDispatch useSelector

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,9 +8,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
